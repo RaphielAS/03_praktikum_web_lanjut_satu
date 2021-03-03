93714e6addca457fc8c2e7ae1729749fb3d21a99
RaphielAS <7d13ilhamnurfebrianto@gmail.com>
2021-03-03 11:54:17 +0700

    Menambah view blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,17 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <a href="http://localhost:8000/category">halaman produk</a><br>
+    <a href="http://localhost:8000/berita">halaman news</a><br>
+    <a href="http://localhost:8000/program">halaman program</a><br>
+    <a href="http://localhost:8000/abot">halaman about us</a><br>
+    <a href="http://localhost:8000/con">halaman contact us</a><br>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><a href="https://www.educastudio.com/">https://www.educastudio.com/</a>
+</body>
+</html>
